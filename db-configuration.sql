90263580143aa96355f4be99fe94644a0d80147e
DoraNicole <tilica_dora@yahoo.com>
2021-05-26 20:48:01 +0300

    Update db-configuration.sql

--- db-configuration.sql
+++ db-configuration.sql
@@ -1,3 +1,6 @@
 create database db_rent_a_car;
 create user 'admin' identified by 'admin123';
 grant all on db_rent_a_car.* to 'admin';
+create table users;
+create table orders;
+create table cars;
